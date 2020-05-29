ff29af8c2d7c24df559036d9485a3d260b5f7a37
Aeternil <66130078+Aeternil@users.noreply.github.com>
2020-05-29 18:30:29 +0300

    Create Project_core.py
    The core file

---
+++ Project_core.py
@@ -0,0 +1,17 @@
+print ("Wellcome to your post-quarantine activity manager!")
+while (True):
+    print("You can 'add' a new activity, 'edit' an existing activity, 'delete' an activity, 'search' for anything specific or 'exit'")
+    print ("What would you like to do?/n")
+    ans = str(input())
+    if (ans == "exit"):
+        break
+    elif (ans == "add"):
+        pass
+    elif (ans == "edit"):
+        pass
+    elif (ans == "delete"):
+        pass
+    elif (ans == "search"):
+        pass
+    else:
+        pass
