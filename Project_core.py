d9e17f386ee405eeb6235c8194c53a53a25848ff
Aeternil <66130078+Aeternil@users.noreply.github.com>
2020-05-29 22:53:31 +0300

    Update Project_core.py

--- Project_core.py
+++ Project_core.py
@@ -1,17 +1,20 @@
 print ("Wellcome to your post-quarantine activity manager!")
 while (True):
     print("You can 'add' a new activity, 'edit' an existing activity, 'delete' an activity, 'search' for anything specific or 'exit'")
-    print ("What would you like to do?/n")
+    print ("What would you like to do?")
     ans = str(input())
     if (ans == "exit"):
+        print("Closing down")
         break
     elif (ans == "add"):
-        pass
+        print("What do you want to do after quarantine?")
+        ans = str(input())
+        
     elif (ans == "edit"):
         pass
     elif (ans == "delete"):
         pass
     elif (ans == "search"):
         pass
-    else:
-        pass
+    else:     
+        print("It seems your input was incorrect, please try again")
