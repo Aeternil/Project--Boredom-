2f4442b9cb25351abb92f87fb1788b0648c24a60
ValDeSantis <66169856+ValDeSantis@users.noreply.github.com>
2020-06-02 01:04:35 +0300

    Add files via upload

--- Project_core.py
+++ Project_core.py
@@ -1,15 +1,29 @@
+import openpyxl
+data = openpyxl.load_workbook('data.xlsx')
+Sheet = data.active
 print ("Wellcome to your post-quarantine activity manager!")
 while (True):
     print("You can 'add' a new activity, 'edit' an existing activity, 'delete' an activity, 'search' for anything specific or 'exit'")
     print ("What would you like to do?")
     ans = str(input())
     if (ans == "exit"):
+        data.save('data.xlsx')
         print("Closing down")
         break
+    
     elif (ans == "add"):
         print("What do you want to do after quarantine?")
         ans = str(input())
-        
+        flag = False
+        for cell in Sheet['A']:
+            if (cell.value == ans):
+                flag = True
+                break
+        if (flag):
+            print("This activity is already in the database")
+        else:
+            Sheet['A'+ str(len(Sheet['A'])+1)] = ans
+            print("Data saved")
     elif (ans == "edit"):
         pass
     elif (ans == "delete"):
