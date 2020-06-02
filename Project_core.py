a14e80504dc48254443cb19913f1fb1520abf58e
Aeternil <66130078+Aeternil@users.noreply.github.com>
2020-06-02 13:16:11 +0300

    Update Project_core.py

--- Project_core.py
+++ Project_core.py
@@ -1,20 +1,58 @@
+import openpyxl
+data = openpyxl.load_workbook('data.xlsx')
+Sheet = data.active
 print ("Wellcome to your post-quarantine activity manager!")
 while (True):
     print("You can 'add' a new activity, 'edit' an existing activity, 'delete' an activity, 'search' for anything specific or 'exit'")
     print ("What would you like to do?")
     ans = str(input())
     if (ans == "exit"):
+        data.save('data.xlsx')
         print("Closing down")
         break
     elif (ans == "add"):
         print("What do you want to do after quarantine?")
         ans = str(input())
-        
+        none_count = None
+        flag = False
+        for cell in Sheet['A']:
+            if (cell.value == None):
+                none_count = cell
+            if (cell.value == ans):
+                flag = True
+                break
+        if (flag):
+            print("This activity is already in the database")
+        else:
+            if (none_count != None):
+                none_count.value = ans
+            else:
+                Sheet['A'+ str(len(Sheet['A'])+1)] = ans
+            print("Data saved")
     elif (ans == "edit"):
-        pass
+        print("Please type in the name of the activity that you want to edit and what you want to change it to (with a space bar in between)")
+        ans, n_ans = map(str, input().split())
+        for row in Sheet.iter_rows():
+            for cell in row:
+                if (cell.value == ans):
+                    cell.value = n_ans
     elif (ans == "delete"):
-        pass
+        print("Please type in the name of the activity that you want to delete")
+        ans = str(input())
+        flag = True
+        for row in Sheet.iter_rows():
+            for cell in row:
+                if (cell.value == ans):
+                    flag = False
+                    cell.value = None
+        if (flag):
+            print("There is no such activity")
     elif (ans == "search"):
-        pass
+        print ("Please type in the first few letters of the activity, that you are searching for")
+        ans = str(input())
+        for row in Sheet.iter_rows():
+            for cell in row:
+                if (ans in cell.value):
+                    print(cell.value)
     else:     
         print("It seems your input was incorrect, please try again")
